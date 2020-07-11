20070268d3068181a567952a9e93e0ffaab8d655
DESKTOP-MLEK48K\dhgia <dangkynguyhiem@gmail.com>
2020-07-11 16:41:40 +0700

    t

--- basics.py
+++ basics.py
@@ -4,13 +4,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 
 chrome_path = which("chromedriver")
 
 driver = webdriver.Chrome(executable_path=chrome_path,options=chrome_options)
 driver.get("https://duckduckgo.com")
-search_input = driver.find_element_by_id('search_form_input_homepage')
+search_input = driver.find_element_by_xpath(r'//input[contains(@id,"search_form_input_homepage")]')
 search_input.send_keys("My User Agent")
 
 # search_btn = driver.find_element_by_id('search_button_homepage')
@@ -20,4 +20,4 @@ search_input.send_keys(Keys.ENTER)
 
 print(driver.page_source)
 
-driver.close()
+# driver.close()
